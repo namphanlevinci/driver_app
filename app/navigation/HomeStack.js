190608c56ab2f791af4afb5f30dc751de607b955
Lan Pham <lan.pham@levincigroup.com>
2020-12-14 21:50:22 +0700

    change key code push & build release

--- app/navigation/HomeStack.js
+++ app/navigation/HomeStack.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import {
@@ -10,10 +10,40 @@ import {
   ScreenName,
 } from '@screen';
 import AccountDrawer from './DrawerTab';
+import { AppState } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { deliveryOrderList, recentlyOrderList } from '@slices/order';
+import { notification } from '@slices/notification';
 
 const Stack = createStackNavigator();
 
 function HomeStack(props) {
+  const dispatch = useDispatch();
+  const appState = useRef(AppState.currentState);
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
+
+  useEffect(() => {
+    AppState.addEventListener('change', _handleAppStateChange);
+
+    return () => {
+      AppState.removeEventListener('change', _handleAppStateChange);
+    };
+  }, []);
+
+  const _handleAppStateChange = (nextAppState) => {
+    if (
+      appState.current.match(/inactive|background/) &&
+      nextAppState === 'active'
+    ) {
+      // console.log('AppState', appState.current);
+      dispatch(deliveryOrderList());
+      dispatch(recentlyOrderList({ page: 1 }));
+      dispatch(notification({ type: 'delivery' }));
+    }
+
+    appState.current = nextAppState;
+    setAppStateVisible(appState.current);
+  };
   return (
     <Stack.Navigator headerMode={'none'}>
       <>
