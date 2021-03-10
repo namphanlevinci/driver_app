bb12338f91b06d952b6c95ad794baba0d3dfbff0
Trần Hoàng Nhã <thediffest@gmail.com>
2021-03-10 17:01:37 +0700

    ios config

--- app/graphql/client.js
+++ app/graphql/client.js
@@ -10,9 +10,7 @@ import { getJwtToken } from '@services/AsyncStoreExt';
 import * as NavigationService from '@navigate/NavigationService';
 
 const httpLink = createHttpLink({
-  // uri: 'https://dev.jollibee.levincitest.com/graphql',
-  // uri: 'https://jollibee.test88.info/graphql', // STAGING
-  uri: 'https://jollibee.test88.info/graphql', // PRODUCT
+  uri: Config.GRAPHQL_ENDPOINT,
 });
 
 const authLink = setContext(async (req, { headers }) => {
