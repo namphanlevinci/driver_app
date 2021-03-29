b13184dcb29ade0631bdd36071a5bc950254f70e
Trần Hoàng Nhã <thediffest@gmail.com>
2021-03-29 09:25:46 +0700

    build test flight fail

--- app/screens/index.js
+++ app/screens/index.js
@@ -1,12 +1,12 @@
 import WelcomeScreen from './Welcome';
 import HomeScreen from './Home';
 import ScreenName from './ScreenName';
-import Account from "./Account";
-import Notification from "./Notification";
-import NewOrder from "./NewOrder";
-import OldOrder from "./OldOrder";
-import Signin from "./Signin";
-import Signup from "./Signup";
+import Account from './Account';
+import Notification from './Notification';
+import NewOrder from './NewOrder';
+import OldOrder from './OldOrder';
+import Signin from './Signin';
+import Signup from './Signup';
 
 module.exports = {
   ScreenName,
@@ -17,5 +17,5 @@ module.exports = {
   Account,
   Notification,
   NewOrder,
-  OldOrder
+  OldOrder,
 };
