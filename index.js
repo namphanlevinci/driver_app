fa8a856d7da174ac199bbcd17074870d8be1630c
namphanLEVINCI <nam.phan@levincigroup.com>
2024-06-13 12:06:35 +0700

    new-relic

--- index.js
+++ index.js
@@ -3,10 +3,61 @@
  */
 
 import 'react-native-gesture-handler';
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './app/App';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
+import NewRelic from 'newrelic-react-native-agent';
+import * as appVersion from './package.json';
+import {Platform} from 'react-native';
 // import { enableScreens } from 'react-native-screens';
 // enableScreens();
 
+let appToken;
+
+if (Platform.OS === 'ios') {
+  appToken = 'AA07d494be62f33a432bcf48fe71c013f9522dfa26-NRMA';
+} else {
+  appToken = 'AA4b839eaac4bf77d370ac18ad133f6ac74a432701-NRMA';
+}
+
+const agentConfiguration = {
+  //Android Specific
+  // Optional:Enable or disable collection of event data.
+  analyticsEventEnabled: true,
+
+  // Optional:Enable or disable crash reporting.
+  crashReportingEnabled: true,
+
+  // Optional:Enable or disable interaction tracing. Trace instrumentation still occurs, but no traces are harvested. This will disable default and custom interactions.
+  interactionTracingEnabled: true,
+
+  // Optional:Enable or disable reporting successful HTTP requests to the MobileRequest event type.
+  networkRequestEnabled: true,
+
+  // Optional:Enable or disable reporting network and HTTP request errors to the MobileRequestError event type.
+  networkErrorRequestEnabled: true,
+
+  // Optional:Enable or disable capture of HTTP response bodies for HTTP error traces, and MobileRequestError events.
+  httpResponseBodyCaptureEnabled: true,
+
+  // Optional:Enable or disable agent logging.
+  loggingEnabled: true,
+
+  // Optional:Specifies the log level. Omit this field for the default log level.
+  // Options include: ERROR (least verbose), WARNING, INFO, VERBOSE, AUDIT (most verbose).
+  logLevel: NewRelic.LogLevel.INFO,
+
+  // iOS Specific
+  // Optional:Enable/Disable automatic instrumentation of WebViews
+  webViewInstrumentation: true,
+
+  // Optional:Set a specific collector address for sending data. Omit this field for default address.
+  // collectorAddress: "",
+
+  // Optional:Set a specific crash collector address for sending crashes. Omit this field for default address.
+  // crashCollectorAddress: ""
+};
+
+NewRelic.startAgent(appToken, agentConfiguration);
+NewRelic.setJSAppVersion(appVersion.version);
 AppRegistry.registerComponent(appName, () => App);
